98612b13090dbdc7e718325f42fc773b9c6656c7
shaheer-2000 <shaheer.2000@hotmail.com>
2022-05-14 14:53:39 +0500

    Remove old implementation + Add new checksum and packet utilities implementations

---
+++ lib/checksum.py
@@ -0,0 +1,76 @@
+"""
+Sum-Complement checksum implementation
+"""
+
+"""
+Break packet into smaller chunks of pkt_size,
+then sum these chunks and wrap the overflow bits (add them),
+until the pkt is of pkt_size, then take 1's complement
+
+pkt_size should be in bytes
+"""
+def generate_chksum(pkt: bytes, pkt_size: int=1):
+	sum = 0
+	pkt_int = int(pkt, 2)
+	# 2 ** 8 (256) is 9 bits long, subtract 1 to get all 1s and reduce to pkt_size - 1 bits
+	pkt_bits = 8 * pkt_size
+	pkt_mask = (1 << pkt_bits) - 1
+
+	# sum bits
+	while pkt_int.bit_length():
+		sum += pkt_int & pkt_mask
+		pkt_int >>= pkt_bits
+		# logical left shift
+		# pkt_int = (pkt_int << pkt_bits) & ((1 << pkt_int.bit_length()) - 1)
+
+	_sum = 0
+	# wrap overflow bits and add them as well
+	# assuming max overflow is pkt_size bits
+	while sum.bit_length():
+		_sum += sum & pkt_mask
+		sum >>= pkt_bits
+
+	"""
+	Take 1s complement
+	https://stackoverflow.com/questions/31151107/how-do-i-do-a-bitwise-not-operation-in-python
+	prepends
+	"""
+	complement = (1 << pkt_bits) - 1 - _sum
+
+	# left-pad bits if leading 0s missing, to get to pkt_size
+	return ("".zfill(pkt_bits - complement.bit_length()) + bin(complement)[2:]).encode()
+
+"""
+Break packet into smaller chunks of pkt_size,
+then sum these chunks and wrap the overflow bits (add them),
+verify that the sum is all 1s or 0xFFFF...
+"""
+def verify_chksum(pkt: bytes, pkt_size: int=8):
+	sum = 0
+	pkt_int = int(pkt, 2)
+	# 2 ** 8 (256) is 9 bits long, subtract 1 to get all 1s and reduce to pkt_size - 1 bits
+	pkt_bits = 8 * pkt_size
+	pkt_mask = (1 << pkt_bits) - 1
+
+	# sum bits
+	while pkt_int.bit_length():
+		sum += pkt_int & pkt_mask
+		pkt_int >>= pkt_bits
+		# logical left shift
+		# pkt_int = (pkt_int << pkt_bits) & ((1 << pkt_int.bit_length()) - 1)
+
+	_sum = 0
+	# wrap overflow bits and add them as well
+	# assuming max overflow is pkt_size bits
+	while sum.bit_length():
+		_sum += sum & pkt_mask
+		sum >>= pkt_bits
+
+	# if all 1s, return True
+	return (_sum & pkt_mask) == pkt_mask
+
+if __name__ == "__main__":
+	# b"1001 1001 1110 0010 0010 0100 1000 0100"
+	print(generate_chksum(b"10011001111000100010010010000100", 1))
+	# b"1001 1001 1110 0010 0010 0100 1000 0100" + b"1101 1010"
+	print(verify_chksum(b"1001100111100010001001001000010011011010", 1))
---
+++ lib/packet.py
@@ -0,0 +1,66 @@
+"""
+Packet creation & extraction related utility methods
+"""
+
+import struct
+
+"""
+https://docs.python.org/3/library/struct.html#struct-format-strings
+
+@ = system native size alignment
+Char	|	C Type			|	Python Type		|	Size	|	Expected Value(s)
+H		|	unsigned short	|	integer			|	2		|	(0, 1)
+s		|	char[]			|	bytes			|	*		|	data in bytes
+I		|	unsigned int	|	integer			|	4		|	checksum value
+"""
+SIZE_ALIGNMENT_CHAR = "@"
+SEQ_NUM_CHAR = "H"
+# DATA_LEN_CHAR = "I"
+DATA_CHAR = "s"
+CHKSUM_CHAR	= "I"
+ACK_CHAR = "H"
+SNDPKT_FMT = f"{SIZE_ALIGNMENT_CHAR}{SEQ_NUM_CHAR}{CHKSUM_CHAR}{DATA_CHAR}"
+RCVPKT_FMT = f"{SIZE_ALIGNMENT_CHAR}{SEQ_NUM_CHAR}{ACK_CHAR}{CHKSUM_CHAR}{DATA_CHAR}"
+
+def make_sndpkt(seq_num: int, chksum: int, data: bytes):
+	# append msg
+	sndpkt_fmt = SNDPKT_FMT[:-1] + f"{len(data)}" + SNDPKT_FMT[-1:]
+
+	print(sndpkt_fmt)
+	return struct.pack(sndpkt_fmt, seq_num, chksum, data)
+
+def make_rcvpkt(seq_num: int, ack: int, chksum: int, data: bytes):
+	# append msg
+	rcvpkt_fmt = RCVPKT_FMT[:-1] + f"{len(data)}" + RCVPKT_FMT[-1:]
+
+	return struct.pack(rcvpkt_fmt, seq_num, ack, chksum, data)
+
+def extract_pkt(pkt: bytes, is_sender: bool=True):
+	pkt_header_fmt = SNDPKT_FMT[:-1] if is_sender else RCVPKT_FMT[:-1]
+	pkt_header, pkt = struct.unpack(pkt_header_fmt, pkt[:8]), pkt[8:]
+	data = b""
+	while len(pkt):
+		(s,), pkt = struct.unpack(DATA_CHAR, pkt[:1]), pkt[1:]
+		data += s
+
+	return pkt_header + (data,)
+
+if __name__ == "__main__":
+	seq_num = 0
+	ack = 0
+	data = b"My name is shaheer"
+	chksum = 123
+
+	sndpkt = make_sndpkt(seq_num, chksum, data)
+	rcvpkt = make_rcvpkt(seq_num, ack, chksum, data)
+
+	# print(sndpkt, rcvpkt)
+
+	_sndpkt = extract_pkt(sndpkt)
+	_rcvpkt = extract_pkt(rcvpkt, is_sender=False)
+
+	print(_sndpkt, _rcvpkt)
+
+	assert (seq_num, chksum, data) == _sndpkt
+	assert (seq_num, ack, chksum, data) == _rcvpkt
+
